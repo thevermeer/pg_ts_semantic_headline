3a8de0ad19868fddd3eac6e26b1a4460baddf019
Jeff Vermeer <jeff@semion.ca>
2024-02-27 07:17:13 -0500

    Adds code for replace_multiple_strings function

---
+++ sql/replace_multiple_strings.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION replace_multiple_strings(source_text text, find_array text[], replace_array text[])
+RETURNS text AS
+$$
+DECLARE
+    i integer;
+BEGIN
+	IF (find_array IS NULL) THEN RETURN source_text; END IF;
+    FOR i IN 1..array_length(find_array, 1)
+    LOOP
+        source_text := replace(source_text, find_array[i], replace_array[i]);
+    END LOOP;
+
+    RETURN source_text;
+END;
+$$
+LANGUAGE plpgsql;
